23746332c21af3b9e14507ca6cc5bf5c73992ae1
Abid Salahi <abidsalahi1377@gmail.com>
2020-05-31 11:18:30 -0700

    set up eslint rules and project config for code consistency

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        If you can see this button, it means Material-UI has been installed
+        successfully
+      </Button>
+    </div>
+  );
+};
+
+export default App;
