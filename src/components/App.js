e33dd642ada8aff7c72a15dc138645777166f7b1
Zhen Peng Zhu <zhenpeng.zhu@hotmail.com>
2020-06-03 23:50:53 -0700

    added search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
     <div>
-      <Button variant="contained" color="primary">
-        If you can see this button, it means Material-UI has been installed
-        successfully
-      </Button>
+      <SearchBar></SearchBar>
     </div>
   );
 };
