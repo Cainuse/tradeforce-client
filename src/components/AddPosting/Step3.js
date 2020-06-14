975c6b1e0015e9e194021916bd6aa3ca492730cd
jenessatan <jenessaxtan@gmail.com>
2020-06-13 17:21:02 -0700

    styling for Step 3; clean up

--- src/components/AddPosting/Step3.js
+++ src/components/AddPosting/Step3.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import Chip from "@material-ui/core/Chip";
 
-const useStyles = makeStyles(() => ({
-  tag: {
+const useStyles = makeStyles((theme) => ({
+  tags: {
     fontSize: "20px",
+    margin: theme.spacing(1, 0),
+  },
+  chipContainer: {
+    width: "50%",
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
 }));
 
 const Step3 = (props) => {
-  const { change, requests, addRequest, deleteRequest } = props;
+  const { requests, addRequest, deleteRequest } = props;
 
   const classes = useStyles();
 
@@ -19,7 +26,7 @@ const Step3 = (props) => {
     <React.Fragment>
       <TextField
         required
-        label="Tags"
+        label="Requested Items"
         className={classes.textfield}
         fullWidth
         margin="dense"
@@ -27,18 +34,27 @@ const Step3 = (props) => {
         name="requestedItems"
         defaultValue=""
         onKeyUp={addRequest}
+        placeholder={
+          requests.length >= 5
+            ? "Request limit reached"
+            : "Press enter to add request"
+        }
+        disabled={requests.length >= 5}
       />
-      {requests.map((tag, idx) => (
-        <Chip
-          key={idx}
-          color="primary"
-          onDelete={() => {
-            deleteRequest("requestedItems", idx);
-          }}
-          label={tag}
-          className={classes.tags}
-        />
-      ))}
+      <div className={classes.chipContainer}>
+        {requests.map((tag, idx) => (
+          <Chip
+            key={idx}
+            color="primary"
+            onDelete={() => {
+              deleteRequest("requestedItems", idx);
+            }}
+            label={tag}
+            className={classes.tags}
+            variant="outlined"
+          />
+        ))}
+      </div>
     </React.Fragment>
   );
 };
