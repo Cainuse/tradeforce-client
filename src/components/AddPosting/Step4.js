f9dc9997361c7ac6794527366fec23a3a2c9fe9e
jenessatan <jenessaxtan@gmail.com>
2020-06-14 13:33:23 -0700

    remove unused imports

--- src/components/AddPosting/Step4.js
+++ src/components/AddPosting/Step4.js
@@ -1,15 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
 import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ItemDetailsReview from "./ItemDetailsReview";
 import Chip from "@material-ui/core/Chip";
-import Button from "@material-ui/core/Button";
-import Divider from "@material-ui/core/Divider";
 
 import ExpandableSection from "./ExpandableSection";
 
