aa4a33db1a5a8faa5f1ce85fff50bc4484479d43
jenessatan <jenessaxtan@gmail.com>
2020-06-17 14:16:37 -0700

    set up loading item details from redux store

--- src/redux/reducers/itemDetailReducer.js
+++ src/redux/reducers/itemDetailReducer.js
@@ -1,5 +1,15 @@
 import { initialState } from "../constants/_initialState";
+import { LOAD_ITEM_DETAIL } from "../constants/actionTypes";
+import _ from "lodash";
 
 export const itemDetailReducer = (state = initialState.itemDetail, action) => {
+  if (action === LOAD_ITEM_DETAIL) {
+    let { itemId, postings } = action;
+    let newState = _.find(postings, { id: itemId });
+    if (newState) {
+      return newState;
+    }
+  }
+
   return state;
 };
