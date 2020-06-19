a30d4550e3e2b7ce67250d3af06c29f737bc3d35
jacquelineyin <yin.sangxu@gmail.com>
2020-06-18 18:39:24 -0700

    First iteration of AddOffering: No dynamic changing of form depending on 0 vs 1+ num of ItemsAdded

---
+++ src/components/AddOffering/AddItemForm.js
@@ -0,0 +1,176 @@
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import { categories, conditions } from "../constants/ComponentConstants";
+import OfferingUploadImg from "./OfferingUploadImg";
+import { Button } from "@material-ui/core";
+import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    comment: {
+      outline: "none",
+      marginBottom: "20px",
+    },
+    formSection: {
+      marginBottom: "20px",
+    },
+    formHeader: {
+      color: theme.palette.primary.main,
+      paddingBottom: "10px",
+    },
+    form: {
+      width: "100%",
+      minHeight: "270px",
+      marginBottom: theme.spacing(3),
+      flexGrow: 1,
+    },
+    textField: {
+
+    },
+    addBtnContainer: {
+      justifySelf: "flex-end",
+    },
+    addBtn: {
+      // backgroundColor: theme.palette.primary.main,
+      // color: theme.palette.secondary.main,
+      fontWeight: 300,
+      textTransform: "capitalize",
+      justifySelf: "flex-end",
+      margin: "5px"
+    },
+  })
+)
+
+export const AddItemOffering = (props) => {
+  let classes = useStyles();
+
+  return (
+      <React.Fragment>
+      <Typography variant="h6" className={classes.formHeader}>
+        Add an Item
+      </Typography>
+
+      <Grid
+        container
+        className={classes.form}
+        spacing={1}
+      >
+        <Grid item xs={12}>
+          <TextField
+            required
+            label="Name of Item"
+            className={classes.textField}
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            name="name"
+            // onChange={change}
+            // defaultValue={title}
+            // error={!!errors.title}
+            // helperText={errors.title}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            multiline
+            rows={3}
+            label="Description"
+            className={classes.textfield}
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            name="description"
+            // onChange={change}
+            // defaultValue={description}
+            // error={!!errors.description}
+            // helperText={errors.description}
+          />
+        </Grid>
+        <Grid item xs={3} sm={6}>
+          <TextField
+            required
+            id="outlined-select-category-native"
+            select
+            label="Category"
+            margin="dense"
+            fullWidth
+            SelectProps={{
+              native: true,
+            }}
+            variant="outlined"
+            // onChange={change}
+            name="category"
+            // defaultValue={category}
+            // error={!!errors.category}
+            // helperText={errors.category}
+          >
+            {categories.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+        </Grid>
+        <Grid item xs={3} sm={6}>
+          <TextField
+            required
+            id="outlined-select-condition-native"
+            select
+            label="Condition"
+            margin="dense"
+            fullWidth
+            SelectProps={{
+              native: true,
+            }}
+            variant="outlined"
+            // onChange={change}
+            name="condition"
+            // defaultValue={condition}
+            // error={!!errors.condition}
+            // helperText={errors.condition}
+          >
+            {conditions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            label="Quantity"
+            className={classes.textfield}
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            name="quantity"
+            // defaultValue={quantity}
+            // onChange={change}
+            type="number"
+            inputProps={{ min: "1" }}
+            // error={!!errors.quantity}
+            // helperText={errors.quantity}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <OfferingUploadImg images={props.images}/>
+        </Grid>
+        <Grid container item justify={"flex-end"} xs={12}>
+          <Button
+            color="primary"
+            variant={"contained"}
+            className={classes.addBtn}
+            startIcon={<AddCircleOutlineOutlinedIcon />}
+            onClick={() => null}
+          >
+            Add Item
+          </Button>
+        </Grid>
+      </Grid>
+      </React.Fragment>
+  )
+}
--- src/components/AddOffering/AddOffering.js
+++ src/components/AddOffering/AddOffering.js
@@ -1,6 +1,31 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
+import { connect } from "react-redux";
+import CreateOfferContents from "./CreateOfferContents";
 import { withStyles } from "@material-ui/core/styles";
+import { Tabs, Tab, Typography, Grid, Paper } from "@material-ui/core";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Grid container >
+          <Grid item xs={12}>
+            {children}
+          </Grid>
+        </Grid>
+      )}
+    </div>
+  );
+}
+
 
 const useStyles = (theme) => ({
   paper: {
@@ -11,7 +36,16 @@ const useStyles = (theme) => ({
     outline: "none",
     position: "relative",
   },
+  tabsRoot: {
+    marginBottom: "30px",
+    outline: "display",
+  },
+  modalHeader: {
+    paddingBottom: "20px",
+  },
+
 });
+
 
 /**
  * MAIN: AddOffering Component
@@ -20,13 +54,66 @@ const useStyles = (theme) => ({
 class AddOffering extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      value: 0,
+      addedItems: [],
+      comment: "",
+      nameOfItem: "",
+      quantity: 1,
+      images: [],
+      description: "",
+      category: "",
+      condition: "",
+      tags: [],
+      errors: {
+        addedItems: "",
+        comment: "",
+        nameOfItem: "",
+        description: "",
+        category: "",
+        condition: "",
+        quantity: "",
+      },
+    }
+  }
+
+  handleChangeTab = (event, newValue) => {
+    this.setState({ value: newValue})
   }
 
   render() {
     const { classes } = this.props;
-    return <Paper className={classes.paper}></Paper>;
+
+    return <Paper className={classes.paper}>
+
+      <Typography align="center" variant="h4" className={classes.modalHeader}>
+        Make an Offer
+      </Typography>
+
+      <Tabs className={classes.tabsRoot}
+            value={this.state.value}
+            indicatorColor={"primary"}
+            textColor={"primary"}
+            onChange={this.handleChangeTab}
+      >
+        <Tab label="Create Offering" />
+        <Tab label="Preview" />
+      </Tabs>
+
+      <TabPanel value={this.state.value} index={0} >
+        <CreateOfferContents images={this.state.images}/>
+      </TabPanel>
+      <TabPanel value={this.state.value} index={1} >
+        Item Two
+      </TabPanel>
+
+    </Paper>;
   }
 }
 
-export default withStyles(useStyles)(AddOffering);
-// export default AddOffering;
+const mapStateToProps = (state) => ({
+  currentUser: state.currentUser,
+})
+
+export default connect(mapStateToProps)(withStyles(useStyles)(AddOffering));
