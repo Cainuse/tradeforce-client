cc6d764e3d7095f958b9fde40fb755e3f9982310
jenessatan <jenessaxtan@gmail.com>
2020-06-19 20:24:51 -0700

    style item results page

--- src/components/pages/ItemResults.js
+++ src/components/pages/ItemResults.js
@@ -2,26 +2,50 @@ import React from "react";
 import { connect } from "react-redux";
 import ItemPreview from "../Item/ItemPreview";
 import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import SearchBar from "../Item/SearchBar";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  search: {
+    margin: theme.spacing(7, 0),
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
 
 // this is the page with all the item previews displayed (also includes the result to searching for a specific item)
 const ItemResults = ({ postings }) => {
+  const classes = useStyles();
   return (
-    <Grid container direction={"row"} spacing={3}>
-      {postings.map((item, index) => {
-        return (
-          <Grid key={index} item xs={3}>
-            <ItemPreview
-              id={item.id}
-              title={item.title}
-              datePosted={item.datePosted}
-              location={item.location}
-              images={item.images}
-              postings={postings} //TODO: remove when BE implemented
-            />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <Container className={classes.root}>
+      <div className={classes.search}>
+        <SearchBar />
+      </div>
+      <Grid container direction={"row"} spacing={3}>
+        {postings.map((item, index) => {
+          return (
+            <Grid key={index} item xs={3}>
+              <ItemPreview
+                id={item.id}
+                title={item.title}
+                datePosted={item.datePosted}
+                location={item.location}
+                images={item.images}
+                postings={postings} //TODO: remove when BE implemented
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 };
 
