9e9d58538e090d90d5f0408fff52cd850f2b39af
jenessatan <jenessaxtan@gmail.com>
2020-07-06 19:32:24 -0700

    add middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./components/styles/index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./redux/reducers/_rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
