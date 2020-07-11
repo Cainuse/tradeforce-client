27f4985d1b1a3c2de997fdfb591638ebae81e984
jacquelineyin <yin.sangxu@gmail.com>
2020-07-10 21:59:34 -0700

    Code cleanup: Forgot to remove unused UseEffect import

--- src/components/Login/RegisterForm.js
+++ src/components/Login/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
