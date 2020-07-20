5f0b12ce4e27fc559bed3eddceaaec35c98c50a8
jenessatan <jenessaxtan@gmail.com>
2020-07-20 15:49:34 -0700

    offering actions integration

--- src/redux/actions/offeringActions.js
+++ src/redux/actions/offeringActions.js
@@ -1,8 +1,19 @@
 import axios from "axios";
-import { MAKE_OFFER } from "../constants/actionTypes";
+import {
+  MAKE_OFFER,
+  ACCEPT_OFFER,
+  DECLINE_OFFER,
+  RESCIND_OFFER,
+} from "../constants/actionTypes";
 import {
   ADD_OFFER_SUCCESS,
   ADD_OFFER_ERROR,
+  ACCEPT_OFFER_SUCCESS,
+  ACCEPT_OFFER_ERROR,
+  DECLINE_OFFER_SUCCESS,
+  DECLINE_OFFER_ERROR,
+  RESCIND_OFFER_SUCCESS,
+  RESCIND_OFFER_ERROR,
 } from "../constants/snackbarMessageTypes";
 import { displaySuccess, displayError } from "./snackbarActions";
 import { setLoading } from "./loadingActions";
@@ -14,6 +25,27 @@ const makeOfferSuccess = (offering, postId) => {
     type: MAKE_OFFER,
     offering: offering,
     postId: postId,
+  };
+};
+
+const acceptOfferSuccess = (offering) => {
+  return {
+    type: ACCEPT_OFFER,
+    offering,
+  };
+};
+
+const declineOfferSuccess = (offering) => {
+  return {
+    type: DECLINE_OFFER,
+    offering,
+  };
+};
+
+const rescindOfferSuccess = (offering) => {
+  return {
+    type: RESCIND_OFFER,
+    offering,
   };
 };
 
@@ -29,6 +61,57 @@ export const makeOffer = (offering, postId) => {
       dispatch(displaySuccess(ADD_OFFER_SUCCESS));
     } catch (e) {
       dispatch(displayError(ADD_OFFER_ERROR));
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+};
+
+export const acceptOffer = (offeringId) => {
+  return async (dispatch) => {
+    try {
+      dispatch(setLoading(true));
+      const offeringResponse = await axios.post(
+        `${BASE_URL}/offerings/${offeringId}/accept`
+      );
+      dispatch(acceptOfferSuccess(offeringResponse.data));
+      dispatch(displaySuccess(ACCEPT_OFFER_SUCCESS));
+    } catch (e) {
+      dispatch(displayError(ACCEPT_OFFER_ERROR));
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+};
+
+export const declineOffer = (offeringId) => {
+  return async (dispatch) => {
+    try {
+      dispatch(setLoading(true));
+      const offeringResponse = await axios.post(
+        `${BASE_URL}/offerings/${offeringId}/reject`
+      );
+      dispatch(declineOfferSuccess(offeringResponse.data));
+      dispatch(displaySuccess(DECLINE_OFFER_SUCCESS));
+    } catch (e) {
+      dispatch(displayError(DECLINE_OFFER_ERROR));
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+};
+
+export const rescindOffer = (offeringId) => {
+  return async (dispatch) => {
+    try {
+      dispatch(setLoading(true));
+      const offeringResponse = await axios.post(
+        `${BASE_URL}/offerings/${offeringId}/rescind`
+      );
+      dispatch(rescindOfferSuccess(offeringResponse.data));
+      dispatch(displaySuccess(RESCIND_OFFER_SUCCESS));
+    } catch (e) {
+      dispatch(displayError(RESCIND_OFFER_ERROR));
     } finally {
       dispatch(setLoading(false));
     }
