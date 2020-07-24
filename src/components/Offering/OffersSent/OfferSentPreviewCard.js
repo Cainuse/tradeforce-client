9238d55e71c65db04a921175eb8d10682e16c4f5
jacquelineyin <yin.sangxu@gmail.com>
2020-07-24 01:33:42 -0700

    Implemented offersSent preview cards and implemented rescind icon button functionality

--- src/components/Offering/OffersSent/OfferSentPreviewCard.js
+++ src/components/Offering/OffersSent/OfferSentPreviewCard.js
@@ -6,50 +6,67 @@ import {
   CardHeader,
   CardMedia,
   CardContent,
-  CardActions,
+  CardActions
 } from "@material-ui/core";
-import { Button, Grid } from "@material-ui/core";
+import {
+  Button,
+  IconButton,
+  Grid,
+  Typography
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import RotateLeftIcon from "@material-ui/icons/RotateLeft";
 
 import UserAvatar from "../../User/UserAvatar";
 import defaultProfile from "../../../images/placeholder-profile.png";
-import AcceptIconButton from "../OffersReceived/AcceptIconButton";
-import DeclineIconButton from "../OffersReceived/DeclineIconButton";
 import { openOfferDetailsModal } from "../../../redux/actions/modalActions";
 import { getPostingByIdAsync } from "../../../redux/actions/postingActions";
 import { getUserByIdAsync } from "../../../redux/actions/userActions";
+import RescindIconButton from "./RescindIconButton";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   cardRoot: {
-    width: "280px",
+    width: "280px"
   },
   cardContentRoot: {
     "&:last-child": {
-      paddingBottom: "16px",
-    },
+      paddingBottom: "10px"
+    }
   },
   offerHeader: {
     "& .MuiCardHeader-avatar": {
-      marginRight: "10px",
-    },
+      marginRight: "10px"
+    }
   },
   avatar: {
-    marginRight: "10px",
+    marginRight: "10px"
   },
   previewImg: {
     height: "0",
-    paddingTop: "56.25%",
+    paddingTop: "56.25%"
   },
+  statusLabel: {
+    fontWeight: 500,
+    color: theme.palette.primary.main,
+    paddingRight: "4px"
+  },
+  statusInfo: {
+    textTransform: "lowercase"
+  }
 }));
 
 export const OfferSentPreviewCard = (props) => {
   const dispatch = useDispatch();
   let classes = useStyles();
 
-  let { offer, currentUser } = props;
+  let { offer, currentUser, fns } = props;
+  let { offeredItems } = offer;
 
   let [posting, setPosting] = useState({});
   let [postingOwner, setPostingOwner] = useState({});
+
+  let defaultImg = require("../../../images/default.jpg");
+  let previewImg;
 
   useEffect(() => {
     let source = Axios.CancelToken.source();
@@ -68,7 +85,7 @@ export const OfferSentPreviewCard = (props) => {
         return posting;
       } catch (e) {
         if (Axios.isCancel(e)) {
-          console.log("caught Axios cancel");
+          //do nothing
         } else {
           console.log(e);
         }
@@ -78,9 +95,30 @@ export const OfferSentPreviewCard = (props) => {
     getPostingInfo();
 
     return () => {
-      source.cancel();
+      source.cancel("component OfferingSentPreview was dismounted");
     };
   }, [dispatch, offer.postingId]);
+
+
+  //set previewImage
+  if (offeredItems.length > 0) {
+    let firstItem = offeredItems[0];
+    previewImg = firstItem.images.length > 0 ? firstItem.images[0] : defaultImg;
+  } else {
+    previewImg = defaultImg;
+  }
+
+  /**
+   * set offerInfo
+   *
+   * offerInfo: { offerId, posting, postingOwner }
+   *
+   **/
+  let offerInfo = {
+    offerId: offer._id,
+    posting,
+    postingOwner,
+  }
 
   const handleClickDetails = (contentInfo) => {
     dispatch(openOfferDetailsModal(contentInfo));
@@ -102,31 +140,40 @@ export const OfferSentPreviewCard = (props) => {
         title={`You have made an offer on`}
         subheader={`${postingOwner.userName}'s posting "${posting.title}"`}
       />
-      {/*<CardMedia*/}
-      {/*  className={classes.previewImg}*/}
-      {/*  image={previewImg}*/}
-      {/*  title="Tradeforce"*/}
-      {/*/>*/}
 
-      {/*<CardContent className={classes.cardContentRoot}>*/}
-      {/*  <CardActions>*/}
-      {/*    <Grid container item xs={12} justify={"space-between"}>*/}
-      {/*      <Grid container item xs={4} alignContent={"center"}>*/}
-      {/*        <Button onClick={() => handleClickDetails(contentInfo)}>*/}
-      {/*          Details*/}
-      {/*        </Button>*/}
-      {/*      </Grid>*/}
-      {/*      <Grid container item xs={8} justify={"flex-end"}>*/}
-      {/*        <Grid container item xs={4} justify={"flex-end"}>*/}
-      {/*          <AcceptIconButton fns={fns} offerInfo={offerInfo}/>*/}
-      {/*        </Grid>*/}
-      {/*        <Grid container item xs={4} justify={"flex-end"}>*/}
-      {/*          <DeclineIconButton fns={fns} offerInfo={offerInfo}/>*/}
-      {/*        </Grid>*/}
-      {/*      </Grid>*/}
-      {/*    </Grid>*/}
-      {/*  </CardActions>*/}
-      {/*</CardContent>*/}
+      <CardMedia
+        className={classes.previewImg}
+        image={previewImg}
+        title="Tradeforce"
+      />
+
+      <CardContent className={classes.cardContentRoot}>
+
+        <Grid container item xs={12} style={{ paddingLeft: "16px" }}>
+          <Typography className={classes.statusLabel}>Status:</Typography>
+          <Typography className={classes.statusInfo}>{offer.status}</Typography>
+        </Grid>
+
+
+        <CardActions>
+          <Grid container item xs={12} justify={"space-between"}>
+            <Grid container item xs={6} alignContent={"center"}>
+              <Button onClick={null}>
+                Details
+              </Button>
+            </Grid>
+            <Grid container item xs={6} justify={"flex-end"}>
+              {/*<IconButton onClick={null}>*/}
+              {/*  <RotateLeftIcon className={classes.rescindIcon}/>*/}
+              {/*</IconButton>*/}
+              <RescindIconButton
+                offerInfo={offerInfo}
+                fns={fns}
+              />
+            </Grid>
+          </Grid>
+        </CardActions>
+      </CardContent>
     </Card>
   );
 };
--- src/components/Offering/OffersSent/OffersSent.js
+++ src/components/Offering/OffersSent/OffersSent.js
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Grid } from "@material-ui/core";
-import { OfferingPreview } from "../OffersReceived/OfferingPreview";
 import { OfferSentPreviewCard } from "./OfferSentPreviewCard";
+import ConfirmationDialog from "../../ConfirmationDialog";
+import { rescindOffer } from "../../../redux/actions/offeringActions";
 
 export const OffersSent = (props) => {
+  const dispatch = useDispatch();
   let { currentUser, offersSent } = props;
+
+  let [confirmationOpen, setConfirmationOpen] = useState(false);
+
+  /** offerInfo: { offerId, posting, postingOwner } */
+  let [offerInfoToActUpon, setOfferInfoToActUpon] = useState({});
+
+
+  const handleConfirmationClose = () => {
+    setConfirmationOpen(false);
+  };
+
+  const handleConfirmationOpen = () => {
+    setConfirmationOpen(true);
+  };
+
+  const handleRescindOffer = async () => {
+    let { offerId } = offerInfoToActUpon;
+
+    await dispatch(rescindOffer(offerId));
+    setConfirmationOpen(false);
+  };
+
+  const selectConfirmationToDisplay = () => {
+    let { posting, postingOwner } = offerInfoToActUpon;
+
+    if (confirmationOpen) {
+      console.log("yup");
+      return (
+        <ConfirmationDialog
+          open={confirmationOpen}
+          submitAction={handleRescindOffer}
+          submitName={"Rescind Offer"}
+          dialogMessage={`This action cannot be undone.`}
+          dialogTitle={`Are you sure you want to rescind your offer` +
+          `on ${postingOwner.userName}'s posting "${posting.title}"?`}
+          handleClose={handleConfirmationClose}
+        />
+      );
+    }
+    return null;
+  };
 
   return (
     <Grid container alignContent={"center"} justify={"flex-start"} spacing={2}>
+      {selectConfirmationToDisplay()}
       {offersSent.map((offer, index) => {
         return (
           <Grid item key={index}>
-            <OfferSentPreviewCard currentUser={currentUser} offer={offer} />
+            <OfferSentPreviewCard
+              currentUser={currentUser}
+              offer={offer}
+              fns={{ setOfferInfoToActUpon, handleConfirmationOpen }}
+            />
           </Grid>
         );
       })}
