8c338b557954c15878e01ff9bc7d34d51c4169ef
jenessatan <jenessaxtan@gmail.com>
2020-07-27 20:03:04 -0700

    axios calls & socket initial set up

--- src/components/pages/ChatPage.js
+++ src/components/pages/ChatPage.js
@@ -5,7 +5,7 @@ import Typography from "@material-ui/core/Typography";
 const ChatPage = () => {
   return (
     <Container>
-      <Typography>Whoops!</Typography>
+      <Typography variant="h1">Chat Page</Typography>
     </Container>
   );
 };
