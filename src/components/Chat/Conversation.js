c1817eeda96ff17c86211897a33b6a75482729b1
jenessatan <jenessaxtan@gmail.com>
2020-07-30 16:44:23 -0700

    multiline to chat input

--- src/components/Chat/Conversation.js
+++ src/components/Chat/Conversation.js
@@ -179,6 +179,8 @@ class Conversation extends React.Component {
             className={classes.input}
             size="small"
             value={this.state.message}
+            multiline
+            rowsMax={4}
           />
           <IconButton
             type="submit"
