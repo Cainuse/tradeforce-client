7365f4a435d0b45d09b183cac4d23a98b736e60d
jenessatan <jenessaxtan@gmail.com>
2020-07-31 15:18:31 -0700

    basic 404 page

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,11 +1,48 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "90vh",
+  },
+  mainText: {
+    fontWeight: 400,
+    fontSize: "25vh",
+    color: theme.palette.primary.main,
+  },
+  subText: {
+    fontWeight: 300,
+    margin: theme.spacing(1, 0, 7, 0),
+  },
+}));
 
 const NotFoundPage = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const redirectToHomepage = () => {
+    history.push("/");
+  };
+
   return (
-    <Container>
-      <Typography>Whoops!</Typography>
+    <Container className={classes.container}>
+      <Typography variant="h1" className={classes.mainText}>
+        404
+      </Typography>
+      <Typography variant="h4" className={classes.subText}>
+        {"Oops, sorry we can't find that page!"}
+      </Typography>
+      <Button onClick={redirectToHomepage} variant="outlined" color="primary">
+        Return to Homepage
+      </Button>
     </Container>
   );
 };
