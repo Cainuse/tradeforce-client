06900221fe701c8259337d9dab292541813d874c
jenessatan <jenessaxtan@gmail.com>
2020-08-01 08:07:01 -0700

    chat notifcation indicator

--- src/components/Navigation/NavBar/ActionBtns/ChatBtn.js
+++ src/components/Navigation/NavBar/ActionBtns/ChatBtn.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import SmsOutlinedIcon from "@material-ui/icons/SmsOutlined";
+import ChatSocketServer from "../../../../utils/ChatSocketServer";
+import ChatHttpServer from "../../../../utils/ChatHttpServer";
+import { connect } from "react-redux";
 
 const useStyles = makeStyles(() => ({
   iconBtn: {
@@ -32,21 +35,62 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const ChatBtn = () => {
+const ChatBtn = (props) => {
   const history = useHistory();
   const classes = useStyles();
+  const [unreadCount, setUnreadCount] = useState(0);
+
+  useEffect(() => {
+    async function fetchUnreadCount() {
+      const response = await ChatHttpServer.getUnreadCount(
+        props.currentUser.user._id
+      );
+      setUnreadCount(response.unreadCount);
+    }
+
+    if (window.location.pathname !== "/chat") {
+      fetchUnreadCount();
+    } else {
+      setUnreadCount(0);
+    }
+  }, [unreadCount]);
+
+  useEffect(() => {
+    ChatSocketServer.eventEmitter.on(
+      "add-message-response",
+      incrementUnreadCount
+    );
+
+    return () => {
+      ChatSocketServer.eventEmitter.removeListener(
+        "add-message-response",
+        incrementUnreadCount
+      );
+    };
+  });
+
+  const incrementUnreadCount = () => {
+    if (window.location.pathname !== "/chat") {
+      setUnreadCount(unreadCount + 1);
+    }
+  };
 
   const redirect = () => {
+    setUnreadCount(0);
     history.push({ pathname: "/chat" });
   };
 
   return (
     <IconButton color="inherit" onClick={redirect} className={classes.iconBtn}>
-      <Badge color="primary" variant="dot" visible="false">
+      <Badge color="error" badgeContent={unreadCount}>
         <SmsOutlinedIcon className={classes.chatBtn} />
       </Badge>
     </IconButton>
   );
 };
 
-export default ChatBtn;
+const mapStateToProps = (state) => ({
+  currentUser: state.currentUser,
+});
+
+export default connect(mapStateToProps)(ChatBtn);
--- src/utils/ChatHttpServer.js
+++ src/utils/ChatHttpServer.js
@@ -12,6 +12,31 @@ class ChatHttpServer {
       return error;
     }
   };
+
+  getUnreadCount = async (userId) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/unread/${userId}`);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  markConversationAsRead = async (fromUserId, toUserId) => {
+    try {
+      await axios.patch(`${BASE_URL}/markAllAsRead`, { fromUserId, toUserId });
+    } catch (error) {
+      return error;
+    }
+  };
+
+  markOneAsRead = async (messageId) => {
+    try {
+      await axios.patch(`${BASE_URL}/markOneAsRead`, { messageId });
+    } catch (error) {
+      return error;
+    }
+  };
 }
 
 export default new ChatHttpServer();
