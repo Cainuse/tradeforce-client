2e7f23978e37f590a1c7bc20f7e019131cacec52
jacquelineyin <yin.sangxu@gmail.com>
2020-08-01 10:58:51 -0700

    Yarn lint & Yarn format & Code clean up

--- src/components/Item/ItemPage.js
+++ src/components/Item/ItemPage.js
@@ -1,39 +1,39 @@
 import React, { useEffect } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
-import { useHistory, useLocation, withRouter } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useLocation } from "react-router";
 
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 import ReviewSection from "./ReviewSection";
 import ItemDetailContainer from "./ItemDetailContainer";
-import { getPostingByIdAsync, loadItemDetail } from "../../redux/actions/postingActions";
-import Axios from "axios";
-import { getUserByIdAsync } from "../../redux/actions/userActions";
+import { loadItemDetail } from "../../redux/actions/postingActions";
 
 // Item Details page for in-depth view of offered items
 
-const useStyles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    fontFamily: "Montserrat"
+    fontFamily: "Montserrat",
   },
   buttonHeader: {
     display: "flex",
     justifyContent: "space-between",
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
   },
   divider: {
     marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3)
-  }
-});
+    marginBottom: theme.spacing(3),
+  },
+}));
 
-const ItemPage = ({ classes, itemDetail }) => {
+const ItemPage = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
   const location = useLocation();
+  const itemDetail = useSelector((state) => state.itemDetail);
 
   useEffect(() => {
     async function loadPosting() {
@@ -46,15 +46,14 @@ const ItemPage = ({ classes, itemDetail }) => {
       } catch (error) {
         console.log(error);
       }
-    };
+    }
 
     loadPosting();
-  }, [location, history, dispatch]);
+  }, [dispatch, history, location]);
 
   const redirect = async () => {
     history.goBack();
   };
-
 
   return Object.keys(itemDetail).length !== 0 ? (
     <div>
@@ -62,19 +61,12 @@ const ItemPage = ({ classes, itemDetail }) => {
         <Button onClick={redirect}>&lt; Back</Button>
       </div>
       <Container className={classes.root}>
-        <ItemDetailContainer itemDetail={itemDetail}/>
-        <Divider className={classes.divider}/>
-        <ReviewSection itemDetail={itemDetail}/>
+        <ItemDetailContainer itemDetail={itemDetail} />
+        <Divider className={classes.divider} />
+        <ReviewSection itemDetail={itemDetail} />
       </Container>
     </div>
   ) : null;
-
 };
 
-const mapStateToProps = (state) => ({
-  itemDetail: state.itemDetail
-});
-
-export default connect(mapStateToProps, { loadItemDetail })(
-  withRouter(withStyles(useStyles)(ItemPage))
-);
+export default ItemPage;
